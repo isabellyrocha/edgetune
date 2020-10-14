79d9674e24ee6de50316e6e11b280f2eccc4bc4d
Isabelly Rocha <isabelly.rocha@unine.ch>
2020-10-14 15:07:46 +0000

    Updating POE power script.

--- scripts/poe_power.py
+++ scripts/poe_power.py
@@ -3,18 +3,19 @@ from datetime import datetime
 import subprocess
 import time
 import json
+import argparse
 
 def get_nodename(node_id):
     switch = {
-        11: "chasseral-1",
-        21: "vully-1",
-        38: "chaumont-8"
+        11: "chasseral",
+        21: "vully",
+        38: "chaumont"
     }
     return switch.get(node_id, None)
 
 def get_json(nodename, timestamp, power):
     json_body = [{
-        "measurement": "poe_power/node_utilization",
+        "measurement": "power/node_utilization",
         "tags": {
             "nodename": nodename,
         },
@@ -26,19 +27,19 @@ def get_json(nodename, timestamp, power):
     return json_body
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='POE power reader.')
+    parser.add_argument('-p', '--port', type=int, default=11, help='Port.')
+    parser.add_argument('-f', '--frequency', type=int, default=1, help='Port.')
+    args = parser.parse_args()
+
     client = InfluxDBClient('localhost', 8086, 'root', 'root', 'power')
     while True:
         timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-        p = subprocess.Popen(['./poe', '-j'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(['./poe', '-p', str(args.port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
-        array = out.decode('utf-8').splitlines()
-        for line in array:
-           try:
-                info = json.loads(line.lstrip("b\'").rstrip("\'").lstrip())
-                nodename = get_nodename(info['interface'])
-                if nodename:
-                    json_body = get_json(nodename, timestamp, info['consumed']) 
-                    client.write_points(json_body)
-           except Exception as err:
-             print("Error! ", err)
+        print(out.decode('utf-8'))
+        array = out.decode('utf-8').split(",")
+        nodename = get_nodename(args.port)
+        json_body = get_json(nodename, timestamp, float(array[3])) 
+        client.write_points(json_body)
 #        time.sleep(1)
