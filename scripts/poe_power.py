0ec4d1a498f5289f9bf36256c99a88592105dbed
Isabelly Rocha <isabelly.rocha@unine.ch>
2020-10-14 10:09:45 +0000

    POE power script.

--- scripts/poe_power.py
+++ scripts/poe_power.py
@@ -1,14 +1,44 @@
+from influxdb import InfluxDBClient
+from datetime import datetime
 import subprocess
+import time
+import json
 
 def get_nodename(node_id):
-    switcher = {
+    switch = {
         11: "chasseral-1",
         21: "vully-1",
         38: "chaumont-8"
     }
-    return switcher.get(node_id, "Invalid node id!")
+    return switch.get(node_id, None)
+
+def get_json(nodename, timestamp, power):
+    json_body = [{
+        "measurement": "poe_power/node_utilization",
+        "tags": {
+            "nodename": nodename,
+        },
+        "time": timestamp,
+        "fields": {
+            "value": power
+        }
+    }]
+    return json_body
 
 if __name__ == '__main__':
-    p = subprocess.Popen(['./poe', '-j'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    print(out)
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'power')
+    while True:
+        timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        p = subprocess.Popen(['./poe', '-j'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        array = out.decode('utf-8').splitlines()
+        for line in array:
+           try:
+                info = json.loads(line.lstrip("b\'").rstrip("\'").lstrip())
+                nodename = get_nodename(info['interface'])
+                if nodename:
+                    json_body = get_json(nodename, timestamp, info['consumed']) 
+                    client.write_points(json_body)
+           except Exception as err:
+             print("Error! ", err)
+#        time.sleep(1)
